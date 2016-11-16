cf550c75b28f8aead78436d0c1244979a99d4d4c
Mark Lodato <lodato@google.com>
2016-11-15 23:15:40 -0500

    ipython: resize numpy/ipython linewidth on window resize
    Also reformat the file using yapf.

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,3 +1,6 @@
+import shutil
+import signal
+import sys
 
 c = get_config()
 
@@ -13,9 +16,7 @@ c.InteractiveShellApp.exec_lines = ['autoreload 1']
 # IPython's default readline config remaps the cursor keys to do history
 # searches, which is extremely annoying.  By setting this here, we prevent *all*
 # of the default mappings, the only of which we care about is tab completion.
-c.TerminalInteractiveShell.readline_parse_and_bind = [
-    'tab: complete',
-]
+c.TerminalInteractiveShell.readline_parse_and_bind = ['tab: complete', ]
 
 # Make the IPython prompt look like the regular Python one.
 try:
@@ -26,17 +27,33 @@ except ImportError:
     c.PromptManager.out_template = ''
     c.PromptManager.justify = False
 else:
+
     class BasicPythonPrompt(Prompts):
         """Emulates the regular python interactive prompt."""
+
         def in_prompt_tokens(self, cli=None):
             return [(Token.Prompt, '>>> ')]
+
         def continuation_prompt_tokens(self, cli=None, width=None):
             return [(Token.Prompt, '... ')]
+
         def out_prompt_tokens(self, cli=None):
             return []
+
     c.TerminalInteractiveShell.prompts_class = BasicPythonPrompt
 
 # Set up aliases for shell commands.
-c.AliasManager.user_aliases = [
-    ('git', 'git'),
-]
+c.AliasManager.user_aliases = [('git', 'git'), ]
+
+
+def update_terminal_width(*ignored):
+    """Resize the IPython and numpy printing width to match the terminal."""
+    w, h = shutil.get_terminal_size()
+    c.PlainTextFormatter.max_width = w - 1
+    if 'numpy' in sys.modules:
+        import numpy as np
+        np.set_printoptions(linewidth=w - 5)
+
+
+update_terminal_width()
+signal.signal(signal.SIGWINCH, update_terminal_width)
