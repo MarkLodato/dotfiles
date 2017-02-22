0efda6d3f731f8ea2d70a784a95fd05e9f7d460f
Mark Lodato <lodato@google.com>
2017-02-21 23:12:03 -0500

    ipython: fix pairwise/grouper for py3

--- .ipython/profile_default/startup/60-itertools.py
+++ .ipython/profile_default/startup/60-itertools.py
@@ -6,10 +6,12 @@ def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = itertools.tee(iterable)
     next(b, None)
-    return itertools.izip(a, b)
+    zip = getattr(itertools, 'izip', __builtins__.zip)
+    return zip(a, b)
 
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
-    return itertools.izip_longest(fillvalue=fillvalue, *args)
+    zip_longest = getattr(itertools, 'izip_longest', itertools.zip_longest)
+    return zip_longest(fillvalue=fillvalue, *args)
