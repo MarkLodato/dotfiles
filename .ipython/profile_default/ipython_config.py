c788648a86e0c48202661368dfcefeae8b135de8
Mark Lodato <lodato@google.com>
2018-07-07 17:52:01 -0400

    ipythonrc: use rlipython

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -33,34 +33,11 @@ c.TerminalInteractiveShell.term_title = True
 c.TerminalIPythonApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['autoreload 1']
 
-# IPython's default readline config remaps the cursor keys to do history
-# searches, which is extremely annoying.  By setting this here, we prevent *all*
-# of the default mappings, the only of which we care about is tab completion.
-c.TerminalInteractiveShell.readline_parse_and_bind = ['tab: complete', ]
-
-# Make the IPython prompt look like the regular Python one.
-try:
-    from IPython.terminal.prompts import Prompts, Token
-except ImportError:
-    c.PromptManager.in_template = '>>> '
-    c.PromptManager.in2_template = '... '
-    c.PromptManager.out_template = ''
-    c.PromptManager.justify = False
-else:
-
-    class BasicPythonPrompt(Prompts):
-        """Emulates the regular python interactive prompt."""
-
-        def in_prompt_tokens(self, cli=None):
-            return [(Token.Prompt, '>>> ')]
-
-        def continuation_prompt_tokens(self, cli=None, width=None):
-            return [(Token.Prompt, '... ')]
-
-        def out_prompt_tokens(self, cli=None):
-            return []
-
-    c.TerminalInteractiveShell.prompts_class = BasicPythonPrompt
+# Revert to old readline library so I get all my bindings.
+# Requires `pip install rlipython`.
+# TODO: Configure the prompt to look like regular python (>>>). Look in the git
+# history to see how this used to be done before rlipython.
+c.TerminalIPythonApp.interactive_shell_class = 'rlipython.TerminalInteractiveShell'
 
 # Set up aliases for shell commands.
 c.AliasManager.user_aliases = [('git', 'git'), ]
